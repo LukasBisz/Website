6edef8a75ec833e2dfd2c998915dabd9eb2dc894
LukasBisz <Lukas.Biss@outlook.com>
2024-03-27 02:23:27 +0100

    Add: design cv

--- src/app/pages/cv/cv.component.ts
+++ src/app/pages/cv/cv.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 @Component({
   selector: 'app-cv',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss',
 })
-export class CvComponent {}
+export class CvComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.file();
+  }
+  storage = getStorage();
+
+  file() {
+    const storage = getStorage();
+    getDownloadURL(ref(storage, 'cv/Lebenslauf.pdf')).then((url) => {
+      const xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = (event) => {
+        const blob = xhr.response;
+      };
+      xhr.open('GET', url);
+      xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+      xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+      xhr.setRequestHeader('Content-type', 'application/pdf');
+      xhr.send();
+
+      const embed = document.getElementById('pdf');
+      embed?.setAttribute('src', url);
+    });
+  }
+}
